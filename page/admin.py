88947ec915c2ec0435335bcb12e3acf1236b078d
Ivaylo Bandrov <ivaylobandrov@gmail.com>
2022-01-03 12:52:42 +0200

    first commit, uploding entire project

---
+++ page/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Genre, Book, BookInstance, Author
+
+admin.site.register(Genre)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)   # same as "admin.site.register()"
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
